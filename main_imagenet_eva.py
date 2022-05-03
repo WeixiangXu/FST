8927633c544a489085198ba07753a2a78074f748
wxxu <xuweixiang1996@foxmail.com>
2022-05-03 11:02:35 +0800

    Add files via upload

---
+++ main_imagenet_eva.py
@@ -0,0 +1,128 @@
+from __future__ import print_function
+
+import argparse
+import os
+import shutil
+import time
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+import models.imagenet as customized_models
+import threading
+import math
+from utils import Bar, AverageMeter, accuracy, \
+                    process_parser
+
+from PIL import Image
+
+args = process_parser()
+
+from utils import sparse_util
+
+state = {k: v for k, v in args._get_kwargs()}
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
+use_cuda = torch.cuda.is_available()
+
+if args.manualSeed is None:
+    args.manualSeed = random.randint(1, 10000)
+random.seed(args.manualSeed)
+torch.manual_seed(args.manualSeed)
+if use_cuda:
+    torch.cuda.manual_seed_all(args.manualSeed)
+
+sparse_op = None
+
+def main():
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.test_batch, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    model = customized_models.__dict__[args.arch]()
+    model = torch.nn.DataParallel(model).cuda()
+
+    global sparse_op
+    sparse_op = sparse_util.SparseOp(model)
+
+    if args.resume:
+        print('==> Resuming from checkpoint..')
+        assert os.path.isfile(args.resume), 'Error: no checkpoint directory found!'
+        args.checkpoint = os.path.dirname(args.resume)
+        checkpoint = torch.load(args.resume)
+        model.load_state_dict(checkpoint['state_dict'])
+        sparse_op.mask = checkpoint['weight_mask']
+
+    if args.evaluate:
+        print('\nEvaluation only')
+        criterion = sparse_util.LSR()
+        test_loss, test_acc = test(val_loader, model, criterion, use_cuda)
+        print(' Test Loss:  %.8f, Test Acc:  %.2f' % (test_loss, test_acc))
+        return
+
+def test(val_loader, model, criterion, use_cuda):
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    sparse_op.feedforward_sparse()        
+    model.eval()
+
+    end = time.time()
+    bar = Bar('Processing', max=len(val_loader))
+    for batch_idx, (inputs, targets) in enumerate(val_loader):
+        data_time.update(time.time() - end)
+
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = torch.autograd.Variable(inputs, volatile=True), torch.autograd.Variable(targets)
+
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
+        losses.update(loss.data, inputs.size(0))
+        top1.update(prec1, inputs.size(0))
+        top5.update(prec5, inputs.size(0))
+
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        bar.suffix  = '({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
+                    batch=batch_idx + 1,
+                    size=len(val_loader),
+                    data=data_time.avg,
+                    bt=batch_time.avg,
+                    total=bar.elapsed_td,
+                    eta=bar.eta_td,
+                    loss=losses.avg,
+                    top1=top1.avg,
+                    top5=top5.avg,
+                    )
+        bar.next()
+    sparse_op.restore()        
+        
+    bar.finish()
+    return (losses.avg, top1.avg)
+
+if __name__ == '__main__':
+    main()
